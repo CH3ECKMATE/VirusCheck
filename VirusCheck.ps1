c1a9b7d46ac5883042545fab96a0e935b2631df0
CH3ECKMATE <60855388+CH3ECKMATE@users.noreply.github.com>
2023-10-13 21:27:00 -0600

    VirusCheck.ps1

--- VirusCheck.ps1
+++ VirusCheck.ps1
@@ -1,3 +1,10 @@
+# You need VirusTotal account for APIKEY.
+# CHANGE APIKEY : "XXXX".
+# CHANGE FOLDER : where you want to search. 
+# .XLSX file will be generated. Keep file with this one.
+# Press "q" to save progress
+
+
 Write-Host "Type q to save progress. Please be patient for large dir's :)"
 # Prompt for number of files to scan
 while ($true) {
